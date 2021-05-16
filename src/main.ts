f495edf8436ead9ec4957cf80bec9ca10078c5b5
Abanoub Essam <abanoub.e.mhanna@gmail.com>
2021-05-16 11:14:43 +0200

    WIP

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000
+  const app = await NestFactory.create(AppModule , {cors: true});
+  const port = process.env.PORT || 3000;
+
+  app.setGlobalPrefix('api/v1');
 
   const config = new DocumentBuilder()
     .setTitle('RealTime example')
     .setDescription('The RealTime API description')
     .setVersion('1.0')
     .addBearerAuth()
+    .addServer('/api/v1')
     .build();
     
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config , {ignoreGlobalPrefix: true});
   SwaggerModule.setup('docs', app, document);
 
 
